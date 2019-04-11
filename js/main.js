c5c73ed9c99eb27184d7552cd5c87696b25fb5bd
William Suryawan <suryawan.william@gmail.com>
2019-04-11 19:11:21 +0700

    update component add file and navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 const baseURL = `http://localhost:3000`
+
+const app = new Vue ({
+    el: "#app",
+    data: {
+        isLogin: false
+    },
+    created() {
+        if (localStorage.getItem('access_token')) {
+            this.isLogin = true
+        } else {
+            this.isLogin = false
+        }
+    },
+    methods: {
+
+    }
+})
